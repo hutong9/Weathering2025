3a2fd2a4b66bb33eee078538e17793ba37f86fc1
HuTong <470048408@qq.com>
2025-10-27 13:38:35 +0800

    新增邀请函

--- components/AppNews.vue
+++ components/AppNews.vue
@@ -2,7 +2,7 @@
  * @Author: HuTong 470048408@qq.com
  * @Date: 2025-08-08 13:15:19
  * @LastEditors: HuTong 470048408@qq.com
- * @LastEditTime: 2025-10-14 14:26:23
+ * @LastEditTime: 2025-10-27 13:38:17
  * @FilePath: \Weathering2025\components\AppNews.vue
  * @Description: 
  * 
@@ -17,6 +17,24 @@
       <!-- 右侧开始 -->
       <div>
         <div class="max-w-2xl mx-auto mt-4">
+
+
+
+
+          <div class="flex gap-3 bg-white border border-gray-300 rounded-xl overflow-hidden items-center justify-start">
+            <div class="flex flex-col gap-2 py-2">
+              <a href="/第二届大陆风化会议邀请函.pdf">
+                <div class="inline-flex items-center">
+                  <p class="text-xl hover:text-blue-800 pl-4">邀请函下载</p>
+                  <img src="/img/new.gif" alt="新图标" class="ml-2 inline-block">
+                  <img src="/img/hot.gif" alt="新图标" class="ml-2 inline-block">
+                </div>
+              </a>
+              <p class="text-gray-400 pl-4">
+                2025年10月27日
+              </p>
+            </div>
+          </div>
 
 
           <div class="flex gap-3 bg-white border border-gray-300 rounded-xl overflow-hidden items-center justify-start">
@@ -40,8 +58,8 @@
               <a href="/1号通知.pdf">
                 <div class="inline-flex items-center">
                   <p class="text-xl hover:text-blue-800 pl-4">第一轮会议通知下载</p>
-                  <img src="/img/new.gif" alt="新图标" class="ml-2 inline-block">
-                  <img src="/img/hot.gif" alt="新图标" class="ml-2 inline-block">
+                  <!-- <img src="/img/new.gif" alt="新图标" class="ml-2 inline-block">
+                  <img src="/img/hot.gif" alt="新图标" class="ml-2 inline-block"> -->
                 </div>
               </a>
               <p class="text-gray-400 pl-4">
